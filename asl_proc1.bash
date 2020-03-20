35b1d8af2169c5df6bc19fa3d3043416be2b9f63
award7 <award7@wisc.edu>
2020-03-20 13:17:33 -0500

    revised to make loop in matlab, combined loop and spm function

--- asl_proc1.bash
+++ asl_proc1.bash
@@ -1,21 +1,45 @@
 #!/usr/bin/env bash
 
+#### Constants
 ANALYSIS="asl_proc"
+progname="asl_proc"
 usr="$(whoami)"
-echo "${usr}: Beginning ${ANALYSIS} on $(date)"
+dir=$(pwd)
+shopt -s globstar
+shopt -s nullglob
+# Set path to scripts
+# change as needed
+src_path="/usr/local/bin"
+log_path="/var/log/asl.log"
 
-# Set path to scripts first
- #change as needed
-src_path="C:/Users/atward/Box/Data_Analysis_Transforms/asl/ASL_Analysis_Pipeline"
-log_path="C:/Users/atward/Box/Data_Analysis_Transforms/asl/ASL_Analysis_Pipeline/log.txt"
 
-# logging function
-logger() {
+#### Functions
+
+# error handler
+function err(){
+	case "$1" in
+		1)
+			printf "ERROR: Unknown argument \'%s'\n" "$2";
+			usage;
+			;;
+		2)
+			arr=("$@")
+			printf "ERROR: The following directories are empty:\n";
+			printf "%s\n" "${arr[@]}";
+			printf "Please rectify.\n";
+			;;
+	esac
+	exit
+}
+
+# logger
+function logger(){
     duration="$(timer)"
     printf '%s: %s Successful on %s %s (duration: %s)\n' "$usr" "$ANALYSIS" `date "+%Y-%m-%d %H:%M:%S"` "$duration" >> "$log_path"
 }
 
-timer() {
+# timer
+function timer(){
     if (( $SECONDS > 3600 )) ; then
         let "HOURS=SECONDS/3600"
         let "MINUTES=(SECONDS%3600)/60"
@@ -33,7 +57,7 @@ timer() {
     fi
 }
 
-prompt1() {
+function prompt1(){
     declare -a arr_time=(
     'Time Points:'
     '000- Baseline' 
@@ -56,7 +80,7 @@ prompt1() {
     done
 }
 
-prompt2() {
+function prompt2(){
     TIMEPOINT=""
     until [ "${#TIMEPOINT}" -eq 3 ]; do    
         local prompt="Enter timepoint for ${FOLDER}: "
@@ -65,140 +89,190 @@ prompt2() {
     echo "$TIMEPOINT"
 }
 
+# usage function
+function usage(){
+   echo "
+    Usage: $progname [OPTION]
+    Analyze ASL files
+   
+    Required Arguments:
+     -f, --files          Subject files to analyze
+     -a, -all			  Analyze all files in current directory. Supercedes any -f options
+	    
+    Optional Arguments:
+     -h, --help           Show this help message and exit
+	"
+	exit
+}
 
-read -p "Enter last four digits of study number (i.e. IRB number; press ENTER for all files): " STUDY
+
+#### Main
+
+printf "%s: Beginning %s on %s" "$usr" "$ANALYSIS" "$(date)"
+# read -p "Enter last four digits of study number (i.e. IRB number; press ENTER for all files): " STUDY
 
 
-# checks that all raw files exist
-empty_dir=$(find . -name '*${STUDY}*' -type d -empty)
-if [ -n "$empty_dir" ]; then
-    echo -e "ERROR: The following directories are empty: \n\n ${empty_dir} \n\n Please rectify. \n Canceling."
-    exit 1
-fi
+arr_subj=()
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	case $key in
+		-a|--all)
+        file_arr=(*);
+        break;
+        ;;
+		-f|--files)
+			arr_subj+=("$2"); # make array of subject files
+			shift 2;
+			;;
+		-h|--help)
+			usage;
+			;;
+		*)
+			err 1 "$key";
+			;;
+	esac
+done
 
 
-# create subject file array
-shopt -s nullglob
-arr_subj=(*"$STUDY"*)
+# checks for empty subject directories
+echo "Checking for empty directories..."
+arr_empty_dir=()
+for subject in "${arr_subj[@]}"; do
+	empty_dir=$(find '$subject' -type d -empty 2>/dev/null)
+	if [ -n "$empty_dir" ]; then
+		arr_empty_dir+=("$empty_dir")
+	fi
+done
+err 2 "${arr_empty_dir[@]}"
 
 
 # set up directory structure in folders
 SECONDS=0
 ANALYSIS="dir_setup.bash"
-#source "${src_path}/${ANALYSIS}" "$STUDY"
-for SUBJECT in "${arr_subj[@]}"; do
+for subject in "${arr_subj[@]}"; do
     # get and list all asl folders
-    arr_folders=("$SUBJECT}"/*)
+    arr_folders=("$subject}"/*)
     for FOLDER in "${arr_folders[@]}"; do
         if [[ "$FOLDER"==*"contrast"* ]]; then        
-            echo
-            echo "ASL folders:"
-            printf "%s\n" "${arr_asl[@]}"
-            echo
+            printf "ASL folders:\n" 
+            printf "${arr_asl[@]}"
             prompt1
             # get input from user
             TIMEPOINT=$(prompt2)
             # change name of folder, make subdirectories, move files to raw dir
-            mv "$FOLDER" "./${SUBJECT}/asl_${TIMEPOINT}_mins"
-            mkdir -p "./${SUBJECT}/asl_${TIMEPOINT}_mins"/{raw,proc}
-            find ./"${SUBJECT}/asl_${TIMEPOINT}_mins" -name '*' -type f -exec mv -t ./"${SUBJECT}/asl_${TIMEPOINT}_mins/raw" {} +
+            mv "$FOLDER" "${subject}/asl_${TIMEPOINT}_mins"
+            mkdir "${subject}/asl_${TIMEPOINT}_mins"/{raw,proc}
+            find "${subject}/asl_${TIMEPOINT}_mins" -name '*' -type f -exec mv -t "${subject}/asl_${TIMEPOINT}_mins/raw" {} +
         elif [[ "$FOLDER"==*"T1*" ]]; then
-            mv "$FOLDER" "./${SUBJECT}/t1"
-            mkdir -p "./${SUBJECT}/t1"/{raw,proc}
-            find ./"${SUBJECT}/t1" -name '*' -type f -exec mv -t ./"${SUBJECT}/t1/raw" {} +
+            mv "$FOLDER" "./${subject}/t1"
+            mkdir "./${subject}/t1"/{raw,proc}
+            find "${subject}/t1" -name '*' -type f -exec mv -t "${subject}/t1/raw" {} +
         else
-            mkdir -p "./${SUBJECT}/${FOLDER}"/{raw,proc}
-            find ./"${SUBJECT}/${FOLDER}" -name '*' -type f -exec mv -t ./"${SUBJECT}/${FOLDER}/raw" {} +
+            mkdir "${subject}/${FOLDER}"/{raw,proc}
+            find "${subject}/${FOLDER}" -name '*' -type f -exec mv -t "${subject}/${FOLDER}/raw" {} +
         fi
     done    
-    echo
-    echo "Completed ---- ${SUBJECT}"
-    echo "-----------------------------------------------------"
+    printf "Completed ---- %s \n%s \n" "$subject" "-----------------------------------------------------"
 done
-logger "${usr}" "$log_path" "${ANALYSIS}" "$SECONDS"
+# logger "${usr}" "$log_path" "${ANALYSIS}" "$SECONDS"
+logger "${usr}" "${ANALYSIS}"
 
 
 # change the raw image filenames
 SECONDS=0
 ANALYSIS = "dcm_ext.bash"
-find ./"$STUDY" -name '*' -type f -execdir bash -c 'mv -i -- "$1" "${1// /_}.dcm"' Mover {} \;
-logger "${usr}" "$log_path" "${ANALYSIS}" "$SECONDS"
+echo "Adding .dcm extension to image files..."
+for subject in "${arr_subj[@]}**"; do
+    find $subject -mindepth 1 ! -name '*.*' -type f -execdir bash -c 'mv -i -- "$1" "${1// /_}.dcm"' Mover {} \;
+done
+logger "${usr}" "${ANALYSIS}"
 
-# install dcm2niix on linux vm
+
+# unzip files if needed
+echo "Unzipping files..."
+for subject in "${arr_subj[@]}"; do
+    find $subject -name '*.gz' -type f -exec gunzip -f {} \;
+done
+
+
 # .dcm to .nii conversion
 SECONDS=0
 ANALYSIS="dcm_import.bash"
-# setup dcm2niix path
-# change this as needed
-for SUBJECT in "${arr_subj[@]}"; do
-	dcm2niix -f "t1_raw" -o "./{SUBJECT}/t1/proc" "./${SUBJECT}/t1/raw"
+echo "DICOM to .nii conversion of T1 images..."
+for subject in "${arr_subj[@]}"; do
+    dcm2niix -f "t1_raw" -o "{subject}/t1/proc" "${subject}/t1/raw"
 done
-logger "${usr}" "$log_path" "${ANALYSIS}" "$SECONDS"
+logger "${usr}" "${ANALYSIS}"
 
 
 # afni fmap and pdmap
-# TODO: make afni path an environment variable
-PATH=$PATH:/usr/local/afni
-for SUBJECT in "${arr_subj[@]}"; do
-	arr_folders=("$SUBJECT}"/asl*)
+SECONDS=0
+ANALYSIS="brik.bash"
+echo "Making fmap and pdmap of ASL images..."
+for subject in "${arr_subj[@]}"; do
+	arr_folders=("$subject}"/asl*)
 	for FOLDER in "${arr_folders[@]}"; do
 		#transform raw asl images into AFNI 4D format
 		#produces a .HEAD and .BRIK file
 		#set prefix to a filename that will be appended by "+orig.BRIK"
-		to3d -prefix "${SUBJECT}_${FOLDER}" -fse -time:zt 40 2 1000 seq+z *.dcm
+		to3d -prefix "${subject}_${FOLDER}" -fse -time:zt 40 2 1000 seq+z *.dcm
 
-		# TODO: install dependencies for 3df_pcasl, add path to environment variable
-		PATH=$PATH:/usr/local/bin
 		#Generate quantitative CBF images
 		#make fmap .BRIK and .HEAD files
 		#filename should be the same prefix used in the to3d command
 		#will be appended by "_fmap+orig.BRIK"
-		3df_pcasl -nex 3 -odata "${SUBJECT}_${FOLDER}"
+		3df_pcasl -nex 3 -odata "${subject}_${FOLDER}"
 
 		#Write out the ASL CBF image in .nii format
 		#make a fmap
 		#filename should be the output from the 3df_pcasl command
-		3dcalc -a "${SUBJECT}_${FOLDER}_fmap+orig.[0]" -datum float -expr 'a' -prefix "${SUBJECT}_${FOLDER}_fmap.nii"
+		3dcalc -a "${subject}_${FOLDER}_fmap+orig.[0]" -datum float -expr 'a' -prefix "${subject}_${FOLDER}_fmap.nii"
 
 		#Write out the ASL CBF image in .nii format
 		#make a proton density map
 		#filename should be the output from the 3df_pcasl command
-		3dcalc -a "${SUBJECT}_${FOLDER}_fmap+orig.[1]" -datum float -expr 'a' -prefix "${SUBJECT}_${FOLDER}_pdmap.nii"
+		3dcalc -a "${subject}_${FOLDER}_fmap+orig.[1]" -datum float -expr 'a' -prefix "${subject}_${FOLDER}_pdmap.nii"
 		
 		#move created files into proc folder
-		mv *.nii *.BRIK *.HEAD -t "./${SUBJECT}/${FOLDER}/proc"
+		mv "${subject}/${FOLDER}/"*.nii "${subject}/${FOLDER}/"*.BRIK "${subject}/${FOLDER}/"*.HEAD -t "${subject}/${FOLDER}/proc"
 	done
 done
 
 
 # call spm functions
 # TODO: pass pwd as argument to matlab
-matlab -nodesktop -nodisplay -nojvm -r "spm_functions('$(pwd)'); exit;"
+    # assing pwd to a string then pass it in??
+echo "Calling MATLab for SPM functions..."
+# pass arr_subj to matlab by 'unraveling' the array into a delimited string
+printf -v arr_subj_matlab '%s ' "${arr_subj[@]}"
+matlab -nodesktop -nodisplay -nojvm -r "spm_functions('$arr_subj_matlab', '$dir'); exit;"
 
 
 # TODO: fsl functions
-# TODO: add fsl to environment variable
-PATH=$PATH:/usr/local/fsl
-for SUBJECT in "${arr_subj[@]}"; do
-	for FOLDER in "${arr_folders[@]}"; do
-		# TODO: get mri img files as inputs
-			# do for all mri imgs for QC purposes
-			# raw t1, segmented t1 imgs, smoothed gm, normalized, etc.
-		# TODO: replace "file [display0pts]" with img file name and display options
+echo "Making images in FSL..."
+# for subject in "${arr_subj[@]}"; do
+	# for FOLDER in "${arr_folders[@]}"; do
+		# # TODO: get mri img files as inputs
+			# # do for all mri imgs for QC purposes
+			# # raw t1, segmented t1 imgs, smoothed gm, normalized, etc.
+		# # TODO: replace "file [display0pts]" with img file name and display options
 	
-		name_outfile_ortho="${SUBJECT}_${FOLDER}_ortho"
-		fsleyes render -sortho -of "$name_outfile_ortho" file [display0pts]
+		# name_outfile_ortho="${subject}_${FOLDER}_ortho"
+		# fsleyes render -sortho -of "$name_outfile_ortho" file [display0pts]
 		
-		name_outfile_lb="${SUBJECT}_${FOLDER}_lightbox"
-		fsleyes render -slightbox -of "$name_outfile_lb" file [display0pts]
+		# name_outfile_lb="${subject}_${FOLDER}_lightbox"
+		# fsleyes render -slightbox -of "$name_outfile_lb" file [display0pts]
 		
-		name_outfile_3d="${SUBJECT}_${FOLDER}_3d"
-		fsleyes render -s3d -of "$name_outfile_3d" file [display0pts]
-	done
-done
+		# name_outfile_3d="${subject}_${FOLDER}_3d"
+		# fsleyes render -s3d -of "$name_outfile_3d" file [display0pts]
+	# done
+# done
 
 
 # zip files
-find . ! \( -name "*.nii" -o -name "*.dcm" \) -type f -exec gzip {} \;
+echo "Zipping files..."
+for subject in "${arr_subj[@]}"; do
+    find $subject ! \( -name "*.nii" -o -name "*.dcm" -o -name "*.BRIK" -o -name "*.HEAD" \) -type f -exec gzip {} \;
+done
 
-# TODO: move subject dir out of 'unproc' dir
+printf "%s completed on %s" "$progname" `date "+%Y-%m-%d %H:%M:%S"`
--- segment.m
+++ segment.m
@@ -1,34 +1,50 @@
-function segment(t1_img)
+function segment(passed_arr_subject, working_dir)
 
+[arr_subject] = split(string(passed_arr_subject));
+
+ANALYSIS = 'Segmentation';
+subfolder = "t1/proc";
+fname = "t1_raw.nii";
+for i = 1:length(arr_subject)
+    src_file = fullfile(working_dir, arr_subject(i), subfolder, fname);
+    if isfile(src_file)
+        segment(src_file);
+    end
+    % mv created files to ./t1/proc
+    completed = (i/length(arr_subject))*100;
+    fprintf('%s: Completed %.0f%%\n', ANALYSIS, completed);
+end
+
+function spm_segment(t1_img)
     spm('defaults', 'FMRI');
     spm_jobman('initcfg');
     clear matlabbatch;
-
-    matlabbatch{1}.spm.spatial.preproc.channel.vols = {[t1_img,',1']} %analyze raw t1 img
+    tpm_img = '/usr/local/MATLAB/spm12/tpm/TPM.nii';
+    matlabbatch{1}.spm.spatial.preproc.channel.vols = {[t1_img,',1']}; % analyze raw t1 img
     matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
     matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
-    matlabbatch{1}.spm.spatial.preproc.channel.write = [0 1]; %save bias corrected
-    matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'C:\Users\atward\Documents\MATLAB\spm12\tpm\TPM.nii,1'};
+    matlabbatch{1}.spm.spatial.preproc.channel.write = [0 1]; % save bias corrected
+    matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {[tpm_img, ',1']}; %
     matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
     matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'C:\Users\atward\Documents\MATLAB\spm12\tpm\TPM.nii,2'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {[tpm_img, ',2'};
     matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
     matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'C:\Users\atward\Documents\MATLAB\spm12\tpm\TPM.nii,3'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {[tpm_img, ',3'};
     matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
     matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'C:\Users\atward\Documents\MATLAB\spm12\tpm\TPM.nii,4'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {[tpm_img, ',4'};
     matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
     matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'C:\Users\atward\Documents\MATLAB\spm12\tpm\TPM.nii,5'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {[tpm_img, ',5'};
     matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
     matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'C:\Users\atward\Documents\MATLAB\spm12\tpm\TPM.nii,6'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {[tpm_img, ',6'};
     matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
     matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
@@ -38,8 +54,6 @@ function segment(t1_img)
     matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
     matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
     matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
-    matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1]; %save forward deformation
-
-    spm_jobman('run', matlabbatch)
-
+    matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1]; % save forward deformation
+    spm_jobman('run', matlabbatch);
 end
