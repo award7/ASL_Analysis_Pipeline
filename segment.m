ae080673485312473c1f449f2e6e49c3a8841e7c
award7 <award7@wisc.edu>
2020-03-20 20:43:15 -0500

    Update segment.m
    spm IO testing v1

--- segment.m
+++ segment.m
@@ -1,18 +1,19 @@
 function segment(passed_arr_subject, working_dir)
 
-[arr_subject] = split(string(passed_arr_subject));
+	[arr_subject] = split(string(passed_arr_subject));
 
-ANALYSIS = 'Segmentation';
-subfolder = "t1/proc";
-fname = "t1_raw.nii";
-for i = 1:length(arr_subject)
-    src_file = fullfile(working_dir, arr_subject(i), subfolder, fname);
-    if isfile(src_file)
-        segment(src_file);
-    end
-    % mv created files to ./t1/proc
-    completed = (i/length(arr_subject))*100;
-    fprintf('%s: Completed %.0f%%\n', ANALYSIS, completed);
+	ANALYSIS = 'Segmentation';
+	subfolder = "t1/proc";
+	fname = "t1_raw.nii";
+	for i = 1:length(arr_subject)
+		src_file = fullfile(working_dir, arr_subject(i), subfolder, fname);
+		if isfile(src_file)
+			spm_segment(src_file);
+		end
+		% mv created files to ./t1/proc
+		completed = (i/length(arr_subject))*100;
+		fprintf('%s: Completed %.0f%%\n', ANALYSIS, completed);
+	end
 end
 
 function spm_segment(t1_img)
@@ -28,23 +29,23 @@ function spm_segment(t1_img)
     matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
     matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {[tpm_img, ',2'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {[tpm_img, ',2']};
     matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
     matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {[tpm_img, ',3'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {[tpm_img, ',3']};
     matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
     matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {[tpm_img, ',4'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {[tpm_img, ',4']};
     matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
     matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {[tpm_img, ',5'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {[tpm_img, ',5']};
     matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
     matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [1 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
-    matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {[tpm_img, ',6'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {[tpm_img, ',6']};
     matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
     matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
     matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
@@ -55,5 +56,15 @@ function spm_segment(t1_img)
     matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
     matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
     matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1]; % save forward deformation
+	
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(1) = cfg_dep('Segment: Seg Params', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','param', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(2) = cfg_dep('Segment: Bias Corrected (1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','channel', '()',{1}, '.','biascorr', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(3) = cfg_dep('Segment: c1 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','c', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(4) = cfg_dep('Segment: c2 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','c', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(5) = cfg_dep('Segment: c3 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{3}, '.','c', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(6) = cfg_dep('Segment: c4 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{4}, '.','c', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(7) = cfg_dep('Segment: c5 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{5}, '.','c', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.files(8) = cfg_dep('Segment: Forward Deformations', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','fordef', '()',{':'}));
+	matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_move.action.moveto = {'/home/atward/Desktop'};
     spm_jobman('run', matlabbatch);
 end
