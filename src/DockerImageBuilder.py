7ff5e6ba9445a4affc4e57d71827a88995d0a84e
Aaron Ward <award7@wisc.edu>
2021-03-27 13:19:17 -0500

    added github addresses to images

--- src/DockerImageBuilder.py
+++ src/DockerImageBuilder.py
@@ -6,10 +6,33 @@ class Builder:
     def __init__(self):
         self.client = docker.from_env()
 
-    def dcm2niix(self):
-        # TODO: pull dockerfile from github
-        pass
+    def afni(self):
+        path = "https://raw.githubusercontent.com/award7/ASL_Analysis_Pipeline/v2/docker/afni/Dockerfile"
+        tag = "asl/afni"
+        self._build(path, tag)
 
+    def dcm2niix(self):
+        # TODO: move to yaml to avoid hardcoding here?
+        path = "https://raw.githubusercontent.com/award7/ASL_Analysis_Pipeline/v2/docker/dcm2niix/Dockerfile"
+        tag = "asl/dcm2niix"
+        self._build(path, tag)
+
+    def fsl(self):
+        path = "https://raw.githubusercontent.com/award7/ASL_Analysis_Pipeline/v2/docker/fsl/Dockerfile"
+        tag = "asl/fsl"
+        self._build(path, tag)
+
+    def spm(self):
+        path = "https://raw.githubusercontent.com/award7/ASL_Analysis_Pipeline/v2/docker/spm/Dockerfile"
+        tag = "asl/spm"
+        self._build(path, tag)
+
+    def _build(self, path: str, tag: str) -> None:
+        self.client.images.build(
+            path=path,
+            tag=tag,
+            rm=True
+        )
 
 if __name__ == '__main__':
     Builder()
