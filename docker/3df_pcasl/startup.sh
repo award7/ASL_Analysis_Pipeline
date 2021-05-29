7c91983b48df97d9315a749a3cd3da979b38baf4
Aaron Ward <award7@wisc.edu>
2021-05-28 20:50:53 -0500

    Create startup.sh

---
+++ docker/3df_pcasl/startup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+while [[ $# -gt 0 ]]
+do
+    key = "$1"
+    
+    case "$key" in 
+        -nex)
+            NEX="$2"
+            shift;
+            shift;
+        -odata)
+            ODATA="$2"
+            shift;
+            shift;
+    esac
+done
+
+/3df_pcasl/3df_pcasl -nex "$NEX" -odata "$ODATA"
