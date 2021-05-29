b6cd0e3aee08af56f22291fe0298152214566868
Aaron Ward <award7@wisc.edu>
2021-05-28 21:32:18 -0500

    removed flag parsing

--- docker/3df_pcasl/startup.sh
+++ docker/3df_pcasl/startup.sh
@@ -1,21 +1,3 @@
 #!/usr/bin/bash
 
-while [[ $# -gt 0 ]]
-do
-    key = "$1"
-    
-    case "$key" in 
-        -nex)
-            NEX="$2"
-            shift
-            shift
-            ;;
-        -odata)
-            ODATA="$2"
-            shift
-            shift
-            ;;
-    esac
-done
-
-/3df_pcasl/3df_pcasl -nex "$NEX" -odata "$ODATA"
+exec /3df_pcasl/3df_pcasl -nex "$1" -odata "$2"
