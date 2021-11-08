57fa41919615a7492230ac5ad027f246f42a152b
schragelab <schragelab@education.wisc.edu>
2021-11-08 15:55:07 -0800

    cleaning up unnecessary files for airflow

--- asl_utils/condense_id.py
+++
@@ -1,6 +0,0 @@
-from re import sub
-
-
-def condense_id(subj_id: str) -> str:
-    # use regex to remove any hypens, underscores, or spaces from subject id
-    return sub('[-_ ]', '', subj_id)
--- src/runConfirmation.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-### highlights green in stdout
-function runConfirmation() {
-    echo "$(tput smso; tput setaf 2)$1$(tput sgr 0)"; echo
-}
-
-runConfirmation "$1"
