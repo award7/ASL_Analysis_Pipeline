31f247f784f18e597f14bd35f6bd6c374bec1312
schragelab <schragelab@education.wisc.edu>
2021-11-12 16:37:12 -0800

    add task to create proc folder; template dag_run.conf

--- dags/asl_t1_processing_dag.py
+++ dags/asl_t1_processing_dag.py
@@ -1,15 +1,29 @@
 from airflow import DAG
-from airflow.utils.task_group import TaskGroup
 from operators.matlab_operator import MatlabOperator
+from airflow.operators.python import PythonOperator
 from operators.docker_templated_mounts_operator import DockerTemplatedMountsOperator
 from datetime import datetime
 import matlab
+from utils.utils import make_dir
 
 
 # todo: set default args dict
 default_args = {}
 
 with DAG(dag_id='t1-processing', schedule_interval=None, start_date=datetime(2021, 11, 1), catchup=False) as dag:
+    # this is where analyzed files (.nii, .BRIK, etc.) will be stored
+    make_proc_t1_path = PythonOperator(
+        task_id='make-proc-t1-path',
+        python_callable=make_dir,
+        op_kwargs={
+            'path': [
+                "{{ var.value.asl_proc_path }}",
+                "{{ dag_run.conf['subject_id'] }}",
+                "t1"
+            ]
+        }
+    )
+
     dcm2niix = DockerTemplatedMountsOperator(
         task_id='dcm2niix',
         image='asl/dcm2niix',
@@ -20,22 +34,23 @@ with DAG(dag_id='t1-processing', schedule_interval=None, start_date=datetime(202
         # move the created files from /tmp to the mounted directory /out ->
         # clear the stdout ->
         # echo the filename to stdout so it will be returned as the xcom value
-        command="""/bin/sh -c \'dcm2niix -f %p_%n_%t -o /tmp /in; clear; file=$(find ./tmp -name "*.nii" -type f); mv /tmp/* /out; clear; echo "${file##*/}"\'""",
+        command="""/bin/sh -c \'dcm2niix -f t1_%n -o /tmp /in; clear; file=$(find ./tmp -name "*.nii" -type f); mv /tmp/* /out; clear; echo "${file##*/}"\'""",
         mounts=[
             {
                 'target': '/in',
-                'source': "{{ dag_run.conf['t1_path'] }}",
+                'source': "{{ dag_run.conf['t1_raw_path'] }}",
                 'type': 'bind'
             },
             {
                 'target': '/out',
-                'source': "{{ dag_run.conf['proc_path'] }}",
+                'source': "{{ ti.xcom_pull(task_ids='make-proc-t1-path') }}",
                 'type': 'bind'
             },
         ],
         auto_remove=True,
         do_xcom_push=True
     )
+    make_proc_t1_path >> dcm2niix
 
     """ 
     following segmentation, by default SPM creates a few files prefaced with `c` for each tissue segmentation, a `y`
@@ -52,8 +67,8 @@ with DAG(dag_id='t1-processing', schedule_interval=None, start_date=datetime(202
     return_value6 = skull image (c4*)
     return_value7 = soft tissue image (c5*)
 
-    to get a specific image, ensure that the nargout parameter will encompass the index of the image. E.g. to get 
-    the csf image, nargout must be 6.
+    to get a specific image into an xcom, ensure that the nargout parameter will encompass the index of the image. 
+    E.g. to get the csf image, nargout must be at least 6.
     """
 
     segment_t1 = MatlabOperator(
@@ -61,7 +76,7 @@ with DAG(dag_id='t1-processing', schedule_interval=None, start_date=datetime(202
         matlab_function='segment_t1',
         matlab_function_paths=["{{ var.value.matlab_path_asl }}"],
         op_args=[
-            "{{ dag_run.conf['proc_path'] }}/{{ ti.xcom_pull(task_ids='dcm2niix') }}",
+            "{{ ti.xcom_pull(task_ids='make-proc-t1-path') }}/{{ ti.xcom_pull(task_ids='dcm2niix') }}",
         ],
         nargout=4
     )
